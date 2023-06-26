a9802f47fbba4dd3d96975d33249cd514acd1238
Katest <103419049+Ekaterlna@users.noreply.github.com>
2023-06-26 18:13:27 +0300

    change price

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int price = -10_000;
+        int price = 10_000;
         int miles = service.calculate(price); // должно получиться 500
         System.out.println(miles);
     }
